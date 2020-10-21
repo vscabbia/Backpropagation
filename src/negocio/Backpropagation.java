66446d02801aadf8df08d4280c4f230202a58d15
Roberto da Silva Neto <58692973+RobertoDSN@users.noreply.github.com>
2020-10-21 20:05:21 -0300

    Update Backpropagation.java

--- src/negocio/Backpropagation.java
+++ src/negocio/Backpropagation.java
@@ -127,6 +127,50 @@ public class Backpropagation {
             System.out.println("número de treinamentos " + n + "\n");
         }
     }
-    
+    public double [] testa_backpropagation(double [][]vv0, double [][]vv, double [][] ww0, double [][]ww){
+        System.out.println("peso w0" + ww0);
+        
+        for(int j = 0; j <= 5; j++) {
+            System.out.println("peso v0[" + j + vv0[j][0]);
+            System.out.println("peso v[" + j + vv[j][0]);
+            System.out.println("peso w[" + j + ww[j][0]);
+        }
+        
+        for(int x=0; x < t.length; x++){
+            zz[0][0] = vv0[0][0] + t[x]*vv[0][0];
+            zz[1][0] = vv0[1][0] + t[x]*vv[1][0];
+            zz[2][0] = vv0[2][0] + t[x]*vv[2][0];
+            zz[3][0] = vv0[3][0] + t[x]*vv[3][0];
+            zz[4][0] = vv0[4][0] + t[x]*vv[4][0];
+            zz[5][0] = vv0[5][0] + t[x]*vv[5][0];
+            
+            zzf[0][0] = 2 / (1 + (Math.exp(-zz[0][0]))) - 1;
+            zzf[1][0] = 2 / (1 + (Math.exp(-zz[1][0]))) - 1;
+            zzf[2][0] = 2 / (1 + (Math.exp(-zz[2][0]))) - 1;
+            zzf[3][0] = 2 / (1 + (Math.exp(-zz[3][0]))) - 1;
+            zzf[4][0] = 2 / (1 + (Math.exp(-zz[4][0]))) - 1;
+            zzf[5][0] = 2 / (1 + (Math.exp(-zz[5][0]))) - 1;
+            
+            zzff[0][0] = 0.5 * (1 + zzf [0][0]) * (1 + zzf [0][0]);
+            zzff[1][0] = 0.5 * (1 + zzf [1][0]) * (1 + zzf [1][0]);
+            zzff[2][0] = 0.5 * (1 + zzf [2][0]) * (1 + zzf [2][0]);
+            zzff[3][0] = 0.5 * (1 + zzf [3][0]) * (1 + zzf [3][0]);
+            zzff[4][0] = 0.5 * (1 + zzf [4][0]) * (1 + zzf [4][0]);
+            zzff[5][0] = 0.5 * (1 + zzf [5][0]) * (1 + zzf [5][0]);
+            
+            yy[x] = ww0 + zzf[0][0] * ww[0][0] + zzf[1][0] * ww[1][0] + zzf[2][0] * ww[2][0] + zzf[3][0] * ww[3][0] + zzf[4][0] * ww[4][0] + zzf[5][0] * ww[5][0];
+            
+            yyf[x] = 2 / (1 + (Math.exp(-yy[x]))) - 1;
+        }
+        int k = 0;
+        
+        for(int i = 0; i < 300; i++)
+            Yamanaka[i] = t[i];
+        for(int j = 0; j < 600; j++){
+            Yamanaka[j] = yyf[k];
+            k++;
+        }
+        return Yamanaka
+    }
     
 }
